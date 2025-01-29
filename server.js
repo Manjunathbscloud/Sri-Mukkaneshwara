efcc4effd577b488df116539fcb2018e94918b46
Manjunath Banakar <manjunathbs.cloud@gmail.com>
2025-01-29 15:40:18 +0530

    server files added

--- server.js
+++ server.js
@@ -5,12 +5,6 @@ const port = process.env.PORT || 3000;
 // Serve static files
 app.use(express.static('public'));
 
-// Basic route
-app.get('/api/test', (req, res) => {
-    res.json({ message: 'Server is working!' });
-});
-
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
-
